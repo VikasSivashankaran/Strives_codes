c82e4461cb38d33b58cf8e75a234339709022472
VikasSivashankaran <vikasbmca@gmail.com>
2024-07-08 19:59:28 +0530

    new

--- find largest.py
+++ find largest.py
@@ -1,6 +1,16 @@
-arr = [56,45,12,85,81]
+arr = [56,450,87,12,85]
 max=arr[0]
-for i in range(len(arr)-1):
+for i in range(1,len(arr)):
     if arr[i] > max:
         max=arr[i]
 print(max)
+
+l1=[56,450,87,12,85]
+print(l1)
+for i in range(0, len(l1)):
+   for j in range(i+1, len(l1)):
+        if l1[i] >= l1[j]:
+            temp = l1[i]
+            l1[i] = l1[j]
+            l1[j] = temp
+print(l1,"\n",l1[-3])
