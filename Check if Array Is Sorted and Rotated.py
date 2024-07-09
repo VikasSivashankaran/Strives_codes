9416f5a5d7a6131819a2576fa2d50bf4fdbe1453
VikasSivashankaran <vikasbmca@gmail.com>
2024-07-09 17:50:23 +0530

    new

---
+++ Check if Array Is Sorted and Rotated.py
@@ -0,0 +1,15 @@
+class Solution:
+    def check(self, nums):
+        n = len(nums)
+        count_rotations = 0
+        
+        for i in range(n):
+            if nums[i] > nums[(i + 1) % n]:
+                count_rotations += 1
+        
+        return count_rotations <= 1
+
+# Example usage:
+nums = [3, 4, 5, 1, 2]
+solution = Solution()
+print(solution.check(nums))  # Output: True
